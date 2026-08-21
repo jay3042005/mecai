355096a77d7395c6d213852374e53ff909524874
Arjay <you@example.com>
2026-08-21 13:56:26 +0800

    Fix dashboard startup using npx next directly

--- start-dashboard.bat
+++ start-dashboard.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-cd /d "%~dp0apps\web"
 set "HOSTNAME=0.0.0.0"
 set "PORT=3000"
-call pnpm run dev -- --hostname 0.0.0.0 --port 3000 --allow-dev-origins *
+cd /d "%~dp0apps\web"
+npx next dev --hostname 0.0.0.0 --port 3000
